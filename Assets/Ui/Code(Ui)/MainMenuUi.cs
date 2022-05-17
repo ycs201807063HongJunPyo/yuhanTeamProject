61767da498ecea9c913b27f50dda6fe89df286b1
ycs201807063HongJunPyo <rus3768@gmail.com>
2022-05-17 19:37:51 +0900

    20220517Update

---
+++ Assets/Ui/Code(Ui)/MainMenuUi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUi : MonoBehaviour
+{
+    public void OnClickOnlineButton() {
+        Debug.Log("GameStart");
+    }
+
+    public void OnClickQuitButton() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
