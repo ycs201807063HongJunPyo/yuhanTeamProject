25b5629975413538f674a271827f2525a99e335d
ycs201807063HongJunPyo <rus3768@gmail.com>
2022-05-22 12:24:14 +0900

    update20220522/shotRotationAndshotLifeTime

--- Assets/BulletSc/Bullet.cs
+++ Assets/BulletSc/Bullet.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float maxBulletLifeTime;
-    public float curBulletLifeTime;
-    void Update() {
-        UpdateLifeTime();
-        Dead();
-    }
-    void UpdateLifeTime() {
-        curBulletLifeTime += Time.deltaTime;
-    }
-    void Dead() {
-        if (curBulletLifeTime > maxBulletLifeTime) {
-            Destroy(this);  //this 하면 딱1개 지워주고 gameObject하면 아예 안나옴(이게 삭제되서 그런듯)
-            //일단 두고 나중에 캐릭터할때 같이하기(선 멀티 -> 캐릭터 작업 -> 총 사격 순서로 하기)
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        //경계 벽 충돌
+        if(collision.gameObject.tag == "LimitLine") {
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Characters/PlayerMovement.cs
+++ Assets/Characters/PlayerMovement.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //이동관련 변수
     public float moveSpeed;
     public Rigidbody2D rb;
     private float moveX;
     private float moveY;
     private Vector2 moveDirection;
     private Animator anim;
+
+    //사격관련 변수
+    public GameObject bulletImage;  //총알 이미지
+    public float shotDelay;  //조준 끝(사격)
+    public float curShotDelay;  //조준 중(조준)
+    public float bulletLifeTime;  //총알 생존 시간
+    private int shotFlag;  //방향 플래그 변수
+    private int shotSpeed = 10;  //총알 속도 일괄 조정
 
     void Awake()
     {
@@ -32,6 +41,8 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+        Fire();
+        AimDelay();
     }
 
     // 입력 처리 함수
@@ -39,17 +50,31 @@ public class PlayerMovement : MonoBehaviour
     {
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
-
+        
         moveDirection = new Vector2(moveX, moveY).normalized;
 
         // 애니메이션 세팅
-        if(anim.GetInteger("hAxisRaw") != moveX){
+        if (anim.GetInteger("hAxisRaw") != moveX){
             anim.SetBool("isChange", true);
             anim.SetInteger("hAxisRaw", (int)moveX);
+            //보는 방향 x축
+            if (moveDirection.x > 0 && moveDirection.y == 0) {
+                shotFlag = 1;
+            }
+            else if (moveDirection.x < 0 && moveDirection.y == 0) {
+                shotFlag = 2;
+            }
         }
         else if(anim.GetInteger("vAxisRaw") != moveY){
             anim.SetBool("isChange", true);
             anim.SetInteger("vAxisRaw", (int)moveY);
+            //보는 방향 y축
+            if (moveDirection.x == 0 && moveDirection.y > 0) {
+                shotFlag = 3;
+            }
+            else if (moveDirection.x == 0 && moveDirection.y < 0) {
+                shotFlag = 4;
+            }
         }
         else{
             anim.SetBool("isChange", false);
@@ -61,4 +86,43 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
+
+    //사격 함수
+    void Fire() {
+        //보는 방향이 정확하지 못함(너무 세세하게 shotFlag가 수정되서), 버튼을 천천하고 정확히 1개씩만 눌러서 방향 확인해야함(일단 나가는거에 의의맞춤)
+        //누르면 쏘게하기 Input.GetButton("Fire1")
+        if (curShotDelay > shotDelay) {
+            GameObject bullet = Instantiate(bulletImage, transform.position, transform.rotation);
+            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+            
+            if(shotFlag == 1) {
+                rigid.AddForce(Vector2.right * shotSpeed, ForceMode2D.Impulse);
+            }
+            else if (shotFlag == 2) {
+                rigid.AddForce(Vector2.left * shotSpeed, ForceMode2D.Impulse);
+            }
+            else if (shotFlag == 3) {
+                rigid.AddForce(Vector2.up * shotSpeed, ForceMode2D.Impulse);
+            }
+            else {
+                rigid.AddForce(Vector2.down * shotSpeed, ForceMode2D.Impulse);
+            }
+            //총알 생존시간 지정
+            ///우리가 총으로 처치하긴해도 박스헤드마냥 총알 막 쏘는겜은 아니라서 넉넉하게 시간줬음(나중에 수정할거면 하기)
+            Destroy(bullet, bulletLifeTime);
+            curShotDelay = 0;
+        }
+        else {
+            return;
+        }
+    }
+
+    //사격 재장전 함수
+    void AimDelay() {
+        curShotDelay += Time.deltaTime;
+    }
+
+
 }
+
+
