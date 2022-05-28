aec6d62b7245f50444394a1b6d4fc02c22cd7c01
ycs201807063HongJunPyo <rus3768@gmail.com>
2022-05-28 10:46:34 +0900

    2022/05/28_tempUpdate

--- Assets/BulletSc/Bullet.cs
+++ Assets/BulletSc/Bullet.cs
@@ -1,10 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Bullet : MonoBehaviour
-{
+public class Bullet : NetworkBehaviour {
 
+    public Rigidbody2D rig;
+    private Vector3 dir;
+    [SyncVar]
+    public int bulletSpeed;
+    void Start() => Destroy(gameObject, 3f);
+
+    void FixedUpdate() {
+        //transform.position += dir * bulletSpeed * Time.deltaTime;
+        rig.AddForce(dir * bulletSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        
+    }
+    [Command]
+    public void CmdShotUpdate(int shotFlag) {
+        RpcShotUpdate(shotFlag);
+    }
+
+    [ClientRpc]
+    public void RpcShotUpdate(int shotFlag) {
+        if (shotFlag == 1) {
+            //rig.AddForce(Vector2.right * bulletSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            dir = Vector3.right;
+        }
+        else if (shotFlag == 2) {
+            //rig.AddForce(Vector2.left * bulletSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            dir = Vector3.left;
+        }
+        else if (shotFlag == 3) {
+            //rig.AddForce(Vector2.up * bulletSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            dir = Vector3.up;
+        }
+        else if (shotFlag == 4){
+            //rig.AddForce(Vector2.down * bulletSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            dir = Vector3.down;
+        }
+        Debug.Log(shotFlag);
+    }
     void OnTriggerEnter2D(Collider2D collision) {
         //경계 벽 충돌
         if(collision.gameObject.tag == "LimitLine") {
--- Assets/Character/Sprites/PlayerMovement.cs
+++ Assets/Character/Sprites/PlayerMovement.cs
@@ -14,16 +14,16 @@ public class PlayerMovement : NetworkBehaviour
     public float moveSpeed;
 
     //사격관련 변수
-    public GameObject bulletImage;  //총알 이미지
-    public float bulletLifeTime;  //총알 생존 시간
+    
     private int shotFlag;  //방향 플래그 변수
-    private int shotSpeed = 10;  //총알 속도 일괄 조정
     [SyncVar]
     public float shotDelay;  //조준 끝(사격)
     [SyncVar]
     public float curShotDelay;  //조준 중(조준)
-
     
+
+
+
 
     void Awake()
     {
@@ -33,6 +33,7 @@ public class PlayerMovement : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         //카메라 조정 코드
         if (hasAuthority) {
             Camera cam = Camera.main;
@@ -42,12 +43,6 @@ public class PlayerMovement : NetworkBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void FixedUpdate()
     {
         Move();
@@ -55,7 +50,6 @@ public class PlayerMovement : NetworkBehaviour
         AimDelay();
     }
 
-    
     // 이동 & 애니메이션 함수
     void Move()
     {
@@ -97,7 +91,6 @@ public class PlayerMovement : NetworkBehaviour
     void Fire() {
         //보는 방향이 정확하지 못함(너무 세세하게 shotFlag가 수정되서), 버튼을 천천하고 정확히 1개씩만 눌러서 방향 확인해야함(일단 나가는거에 의의맞춤)
         //누르면 쏘게하기 Input.GetButton("Fire1")
-
         // 총알 발사 방향
         if (moveX > 0 && moveY == 0) {
             // 오른쪽
@@ -117,24 +110,19 @@ public class PlayerMovement : NetworkBehaviour
         }
 
         if (curShotDelay > shotDelay) {
-            GameObject bullet = Instantiate(bulletImage, transform.position, transform.rotation);
-            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
-            
-            if(shotFlag == 1) {
-                rigid.AddForce(Vector2.right * shotSpeed, ForceMode2D.Impulse);
-            }
-            else if (shotFlag == 2) {
-                rigid.AddForce(Vector2.left * shotSpeed, ForceMode2D.Impulse);
-            }
-            else if (shotFlag == 3) {
-                rigid.AddForce(Vector2.up * shotSpeed, ForceMode2D.Impulse);
+           
+            var manager = NetworkRoomManager.singleton as MafiaRoomManager;
+            var bullet = Instantiate(manager.spawnPrefabs[1], transform.position, transform.rotation);
+            NetworkServer.Spawn(bullet);  // 총알 스폰
+
+            if (manager.mode == Mirror.NetworkManagerMode.Host) {
+                bullet.GetComponent<Bullet>().RpcShotUpdate(this.shotFlag);
             }
             else {
-                rigid.AddForce(Vector2.down * shotSpeed, ForceMode2D.Impulse);
+                bullet.GetComponent<Bullet>().CmdShotUpdate(this.shotFlag);
             }
-            //총알 생존시간 지정
-            ///우리가 총으로 처치하긴해도 박스헤드마냥 총알 막 쏘는겜은 아니라서 넉넉하게 시간줬음(나중에 수정할거면 하기)
-            Destroy(bullet, bulletLifeTime);
+            
+
             curShotDelay = 0;
         }
         else {
