aceb31bc456d6841083268fe26468475db585f89
Kimsungwoon <ssmja12@naver.com>
2022-07-04 20:57:14 +0900

    마피아 start버튼

--- Assets/Ui/GameRoom/Scripts/GameRoomPlayerCounter.cs
+++ Assets/Ui/GameRoom/Scripts/GameRoomPlayerCounter.cs
@@ -19,6 +19,8 @@ public class GameRoomPlayerCounter : NetworkBehaviour
         bool isStartable = players.Length >= 3;
         playerCountText.color = isStartable ? Color.white : Color.red;
         playerCountText.text = string.Format("{0} / {1} \n{2}", players.Length, maxPlayer, manager.networkAddress);
+        // 현재 접속한 인원의 수가 최소 인원이상의 경우에 따라 호출 가능하게 해준다, 11장 8:20
+        LobbyUIManager.Instance.SetInteractableStartButton(isStartable);
     }
 
     // Start is called before the first frame update
--- Assets/Ui/GameRoom/Scripts/LobbyUIManager.cs
+++ Assets/Ui/GameRoom/Scripts/LobbyUIManager.cs
@@ -13,6 +13,9 @@ public class LobbyUIManager : MonoBehaviour
     private GameRoomPlayerCounter gameRoomPlayerCounter;
     public GameRoomPlayerCounter GameRoomPlayerCounter { get { return gameRoomPlayerCounter; } }
 
+    [SerializeField]
+    private Button startButton; //11장 7:44
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,4 +27,29 @@ public class LobbyUIManager : MonoBehaviour
     {
         
     }
+
+    // 11장
+    public void ActiveStartButton()
+    {
+        startButton.gameObject.SetActive(true);  // start버튼을 보여주는 함수 활성화
+    }
+    // 11장
+    public void SetInteractableStartButton(bool isInteractable)
+    {
+        startButton.interactable = isInteractable; // 버튼의 상호작용을 끄거나 켜주도록 만들어주는 함수
+    }
+    // 11장
+    public void OnClickStartButton()
+    {
+        var players = FindObjectsOfType<MafiaRoomPlayer>(); // 모든 플레이어의 readyToBegin을 true로 변경해서 준비시켜준다
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].readyToBegin = true;
+        }
+
+        // Room Manager가 serverChangeScene함수를 이용해서 Gameplay Scene으로 변경한다
+        var manager = NetworkManager.singleton as MafiaRoomManager;
+        manager.ServerChangeScene(manager.GameplayScene);
+    }
+
 }
